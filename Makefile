ccfb2f8601bc56a4948e88dde87c456c9e69fe25
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2022-12-09 16:55:29 +0200

    Додав окремі скрипти для встановлення та видалення мови

--- Makefile
+++ Makefile
@@ -1,36 +1,15 @@
-.PHONY: all rebuild build install uninstall clean test
+.PHONY: all install uninstall test
 
-APP_NAME=borsch
-BINARY=bin/${APP_NAME}
-LIB_DIR=/usr/local/lib/${APP_NAME}-lang
-BIN_DIR=/usr/local/bin
-
-BUILD_TIME=`LC_ALL=uk_UA.utf8 date '+%b %d %Y, %T'`
-LDFLAGS=-ldflags "-X 'github.com/YuriyLisovskiy/borsch-lang/Borsch/cli/build.Time=${BUILD_TIME}'"
-
-all: build
-
-rebuild: clean build
-
-build:
-	mkdir -p bin/ && \
-	go build ${LDFLAGS} -o ${BINARY} Borsch/cli/main.go
+all: install test
 
 install:
-	mkdir -p ${LIB_DIR}
-	cp -R Lib/ ${LIB_DIR}
-	export BORSCH_LIB="${LIB_DIR}/Lib"
-	cp ${BINARY} ${BIN_DIR}/${APP_NAME}
+	@bash ./Scripts/install.sh
 
 uninstall:
-	rm -rf ${LIB_DIR}
-	rm ${BIN_DIR}/${APP_NAME}
-
-clean:
-	if [ -f ${BINARY} ] ; then rm ${BINARY} ; fi
+	@bash ./Scripts/uninstall.sh
 
 test:
-	go run Borsch/cli/main.go Test/вбудовані/типи/дійсний.борщ
-	go run Borsch/cli/main.go Test/вбудовані/типи/логічний.борщ
-	go run Borsch/cli/main.go Test/вбудовані/типи/рядок.борщ
-	go run Borsch/cli/main.go Test/вбудовані/типи/цілий.борщ
+	@go run Borsch/cli/main.go Test/вбудовані/типи/дійсний.борщ
+	@go run Borsch/cli/main.go Test/вбудовані/типи/логічний.борщ
+	@go run Borsch/cli/main.go Test/вбудовані/типи/рядок.борщ
+	@go run Borsch/cli/main.go Test/вбудовані/типи/цілий.борщ
--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
-APP_NAME=borsch
-LIB_DIR=/usr/local/lib/${APP_NAME}-lang
-BIN_DIR=/usr/local/bin
+ROOT_PACKAGE_NAME="github.com/YuriyLisovskiy/borsch-lang/Borsch"
 
-mkdir -p ${LIB_DIR} && \
-cp -R Lib/ ${LIB_DIR} && \
-mkdir -p ${BIN_DIR} && \
-cp bin/${APP_NAME} ${BIN_DIR}/${APP_NAME} && \
-export BORSCH_LIB="${LIB_DIR}/Lib" && \
-echo "Append 'BORSCH_LIB=${LIB_DIR}/Lib' to your ~/.bash_profile for using this variable permanently."
+APP_NAME=borsch
+BORSCH_HOME=~/borsch
+BORSCH_BIN=${BORSCH_HOME}/bin
+BORSCH_LIB=${BORSCH_HOME}/lib
+
+C_BOLD_PURPLE='\033[1;35m'
+C_BOLD_GREEN='\033[1;32m'
+C_BOLD_BLACK='\033[1;30m'
+NO_COLOR='\033[0m'
+
+BUILD_TIME=$(LC_ALL=uk_UA.utf8 date '+%b %d %Y, %T')
+LDFLAGS="-X '${ROOT_PACKAGE_NAME}/cli/build.Time=${BUILD_TIME}'"
+
+echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Встановлення стандартної бібліотеки...${NO_COLOR}"
+mkdir -p ${BORSCH_LIB}
+cp -R Lib/ ${BORSCH_LIB}
+echo -e "${C_BOLD_GREEN}Готово.${NO_COLOR}"
+echo
+echo Бібліотека міститься в каталозі ${BORSCH_LIB}
+echo
+echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Збірка та встановлення інтерпретатора...${NO_COLOR}"
+mkdir -p ${BORSCH_BIN}
+go build -ldflags "${LDFLAGS}" -o ${BORSCH_BIN}/${APP_NAME} Borsch/cli/main.go
+echo -e "${C_BOLD_GREEN}Готово.${NO_COLOR}"
+echo
+echo Інтерпретатор міститься в каталозі ${BORSCH_BIN}
+echo
+echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Завершення процесу встановлення.${NO_COLOR}"
+echo
+echo У кінець профіля командної оболонки, яку ви використовуєте, додайте рядки:
+echo -e "${C_BOLD_BLACK}  export BORSCH_LIB=${BORSCH_LIB}${NO_COLOR}"
+echo -e "${C_BOLD_BLACK}  alias борщ="${BORSCH_BIN}/${APP_NAME}"${NO_COLOR}"
+echo
+echo Перезапустіть термінал, щоб застосувати зміни. Для цього оновіть профіль. Приклад команди для оболонки Bash:
+echo -e "${C_BOLD_BLACK}  source ~/.bash_profile${NO_COLOR}"
+echo
+echo Для застосування змін в інших оболонках, дізнайтеся самостійно, як це зробити, або просто перезапустіть термінал.
+echo
