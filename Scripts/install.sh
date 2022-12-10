1d1637743aee1a1a678c178c0d1b9613235f5df7
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2022-12-10 15:02:32 +0200

    Покращив скрипти

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -15,28 +15,20 @@ NO_COLOR='\033[0m'
 BUILD_TIME=$(LC_ALL=uk_UA.utf8 date '+%b %d %Y, %T')
 LDFLAGS="-X '${ROOT_PACKAGE_NAME}/cli/build.Time=${BUILD_TIME}'"
 
-echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Встановлення стандартної бібліотеки...${NO_COLOR}"
-mkdir -p ${BORSCH_LIB}
-cp -R Lib/ ${BORSCH_LIB}
-echo -e "${C_BOLD_GREEN}Готово.${NO_COLOR}"
-echo
-echo Бібліотека міститься в каталозі ${BORSCH_LIB}
-echo
-echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Збірка та встановлення інтерпретатора...${NO_COLOR}"
-mkdir -p ${BORSCH_BIN}
-go build -ldflags "${LDFLAGS}" -o ${BORSCH_BIN}/${APP_NAME} Borsch/cli/main.go
-echo -e "${C_BOLD_GREEN}Готово.${NO_COLOR}"
-echo
-echo Інтерпретатор міститься в каталозі ${BORSCH_BIN}
-echo
-echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Завершення процесу встановлення.${NO_COLOR}"
-echo
-echo У кінець профіля командної оболонки, яку ви використовуєте, додайте рядки:
-echo -e "${C_BOLD_BLACK}  export BORSCH_LIB=${BORSCH_LIB}${NO_COLOR}"
-echo -e "${C_BOLD_BLACK}  alias борщ="${BORSCH_BIN}/${APP_NAME}"${NO_COLOR}"
-echo
-echo Перезапустіть термінал, щоб застосувати зміни. Для цього оновіть профіль. Приклад команди для оболонки Bash:
-echo -e "${C_BOLD_BLACK}  source ~/.bash_profile${NO_COLOR}"
-echo
-echo Для застосування змін в інших оболонках, дізнайтеся самостійно, як це зробити, або просто перезапустіть термінал.
-echo
+echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Встановлення стандартної бібліотеки...${NO_COLOR}" && \
+mkdir -p ${BORSCH_LIB} && \
+cp -R Lib/ ${BORSCH_LIB} && \
+echo -e "${C_BOLD_GREEN}Готово.${NO_COLOR}" && echo && \
+echo Бібліотека міститься в каталозі ${BORSCH_LIB} && echo && \
+echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Збірка та встановлення інтерпретатора...${NO_COLOR}" && \
+mkdir -p ${BORSCH_BIN} && \
+go build -ldflags "${LDFLAGS}" -o ${BORSCH_BIN}/${APP_NAME} Borsch/cli/main.go && \
+echo -e "${C_BOLD_GREEN}Готово.${NO_COLOR}" && echo && \
+echo Інтерпретатор міститься в каталозі ${BORSCH_BIN} && echo && \
+echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Завершення процесу встановлення.${NO_COLOR}" && echo && \
+echo У кінець профіля командної оболонки, яку ви використовуєте, додайте рядки: && \
+echo -e "${C_BOLD_BLACK}  export BORSCH_LIB=${BORSCH_LIB}${NO_COLOR}" && \
+echo -e "${C_BOLD_BLACK}  alias борщ="${BORSCH_BIN}/${APP_NAME}"${NO_COLOR}" && echo && \
+echo Перезапустіть термінал, щоб застосувати зміни. Для цього оновіть профіль. Приклад команди для оболонки Bash: && \
+echo -e "${C_BOLD_BLACK}  source ~/.bash_profile${NO_COLOR}" && echo && \
+echo Для застосування змін в інших оболонках, дізнайтеся самостійно, як це зробити, або просто перезапустіть термінал. && echo
--- Scripts/uninstall.sh
+++ Scripts/uninstall.sh
@@ -10,22 +10,17 @@ C_BOLD_GREEN='\033[1;32m'
 C_BOLD_BLACK='\033[1;30m'
 NO_COLOR='\033[0m'
 
-echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Видалення стандартної бібліотеки...${NO_COLOR}"
-rm -rf ${BORSCH_LIB}
-echo -e "${C_BOLD_GREEN}Готово.${NO_COLOR}"
-echo
-echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Видалення інтерпретатора...${NO_COLOR}"
-rm -rf ${BORSCH_BIN}
-echo -e "${C_BOLD_GREEN}Готово.${NO_COLOR}"
-rm -rf ${BORSCH_HOME}
-echo
-echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Завершення процесу видалення.${NO_COLOR}"
-echo
-echo -e "Вилучіть змінну ${C_BOLD_BLACK}BORSCH_LIB${NO_COLOR} та обгортку ${C_BOLD_BLACK}борщ${NO_COLOR} за допомогою наступних команд:"
-echo -e "${C_BOLD_BLACK}  unexport BORSCH_LIB${NO_COLOR}"
-echo -e "${C_BOLD_BLACK}  unalias борщ${NO_COLOR}"
-echo
-echo 'Із профіля командної оболонки вилучіть наступні рядки, якщо вони присутні:'
-echo -e "${C_BOLD_BLACK}  export BORSCH_LIB=${BORSCH_LIB}${NO_COLOR}"
-echo -e "${C_BOLD_BLACK}  alias борщ="${BORSCH_BIN}/${APP_NAME}"${NO_COLOR}"
-echo
+echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Видалення стандартної бібліотеки...${NO_COLOR}" && \
+rm -rf ${BORSCH_LIB} && \
+echo -e "${C_BOLD_GREEN}Готово.${NO_COLOR}" && echo && \
+echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Видалення інтерпретатора...${NO_COLOR}" && \
+rm -rf ${BORSCH_BIN} && \
+echo -e "${C_BOLD_GREEN}Готово.${NO_COLOR}" && \
+rm -rf ${BORSCH_HOME} && echo && \
+echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Завершення процесу видалення.${NO_COLOR}" && echo && \
+echo -e "Вилучіть змінну ${C_BOLD_BLACK}BORSCH_LIB${NO_COLOR} та обгортку ${C_BOLD_BLACK}борщ${NO_COLOR} за допомогою наступних команд:" && \
+echo -e "${C_BOLD_BLACK}  unexport BORSCH_LIB${NO_COLOR}" && \
+echo -e "${C_BOLD_BLACK}  unalias борщ${NO_COLOR}" && echo && \
+echo 'Із профіля командної оболонки вилучіть наступні рядки, якщо вони присутні:' && \
+echo -e "${C_BOLD_BLACK}  export BORSCH_LIB=${BORSCH_LIB}${NO_COLOR}" && \
+echo -e "${C_BOLD_BLACK}  alias борщ="${BORSCH_BIN}/${APP_NAME}"${NO_COLOR}" && echo
