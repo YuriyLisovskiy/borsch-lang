29dff833f51ce36577dc874b8b78f3a4ae339f08
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2022-12-14 23:25:14 +0200

    Оновив скрипти для *nix

--- Scripts/uninstall.sh
+++ Scripts/uninstall.sh
@@ -7,20 +7,37 @@ BORSCH_LIB=${BORSCH_HOME}/lib
 
 C_BOLD_PURPLE='\033[1;35m'
 C_BOLD_GREEN='\033[1;32m'
-C_BOLD_BLACK='\033[1;30m'
+C_BOLD_DEFAULT='\033[1m'
 NO_COLOR='\033[0m'
 
-echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Видалення стандартної бібліотеки...${NO_COLOR}" && \
-rm -rf ${BORSCH_LIB} && \
-echo -e "${C_BOLD_GREEN}Готово.${NO_COLOR}" && echo && \
-echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Видалення інтерпретатора...${NO_COLOR}" && \
-rm -rf ${BORSCH_BIN} && \
-echo -e "${C_BOLD_GREEN}Готово.${NO_COLOR}" && \
-rm -rf ${BORSCH_HOME} && echo && \
-echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_BLACK}Завершення процесу видалення.${NO_COLOR}" && echo && \
-echo -e "Вилучіть змінну ${C_BOLD_BLACK}BORSCH_LIB${NO_COLOR} та обгортку ${C_BOLD_BLACK}борщ${NO_COLOR} за допомогою наступних команд:" && \
-echo -e "${C_BOLD_BLACK}  unexport BORSCH_LIB${NO_COLOR}" && \
-echo -e "${C_BOLD_BLACK}  unalias борщ${NO_COLOR}" && echo && \
+echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_DEFAULT}Видалення стандартної бібліотеки...${NO_COLOR}" && echo && \
+echo "Каталог, де розташована стандартна бібліотека:" && \
+echo -e "${C_BOLD_DEFAULT}  ${BORSCH_LIB}${NO_COLOR}" && echo && \
+read -p "Вкажіть каталог, якщо поточний не співпадає [натисніть enter, щоб залишити поточний]: " borsch_lib_var
+if [ -z "$borsch_lib_var" ]
+then
+  borsch_lib_var=${BORSCH_LIB}
+fi
+
+rm -rf ${borsch_lib_var} && \
+echo && echo -e "${C_BOLD_GREEN}Готово.${NO_COLOR}" && echo && \
+
+echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_DEFAULT}Видалення інтерпретатора...${NO_COLOR}" && echo && \
+echo "Каталог, де розташований інтерпретатор:" && \
+echo -e "${C_BOLD_DEFAULT}  ${BORSCH_BIN}${NO_COLOR}" && echo && \
+read -p "Вкажіть каталог, якщо поточний не співпадає [натисніть enter, щоб залишити поточний]: " borsch_bin_var
+if [ -z "$borsch_bin_var" ]
+then
+  borsch_bin_var=${BORSCH_BIN}
+fi
+
+rm -rf ${borsch_bin_var} && \
+echo && echo -e "${C_BOLD_GREEN}Готово.${NO_COLOR}" && echo && \
+
+echo -e "${C_BOLD_PURPLE}==>${NO_COLOR} ${C_BOLD_DEFAULT}Завершення процесу видалення.${NO_COLOR}" && echo && \
+echo -e "Вилучіть змінну ${C_BOLD_DEFAULT}BORSCH_LIB${NO_COLOR} та обгортку ${C_BOLD_DEFAULT}борщ${NO_COLOR} за допомогою наступних команд:" && \
+echo -e "${C_BOLD_DEFAULT}  unexport BORSCH_LIB${NO_COLOR}" && \
+echo -e "${C_BOLD_DEFAULT}  unalias борщ${NO_COLOR}" && echo && \
 echo 'Із профіля командної оболонки вилучіть наступні рядки, якщо вони присутні:' && \
-echo -e "${C_BOLD_BLACK}  export BORSCH_LIB=${BORSCH_LIB}${NO_COLOR}" && \
-echo -e "${C_BOLD_BLACK}  alias борщ="${BORSCH_BIN}/${APP_NAME}"${NO_COLOR}" && echo
+echo -e "${C_BOLD_DEFAULT}  export BORSCH_LIB=${borsch_lib_var}${NO_COLOR}" && \
+echo -e "${C_BOLD_DEFAULT}  alias борщ="${borsch_bin_var}/${APP_NAME}"${NO_COLOR}" && echo
