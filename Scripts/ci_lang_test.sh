c3a2d3e2e1212c9b6c8297c20492b231420a6757
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2023-03-04 16:20:24 +0200

    Оновив ci_lang_test.sh

--- Scripts/ci_lang_test.sh
+++ Scripts/ci_lang_test.sh
@@ -3,7 +3,7 @@
 INTERPRETER_BIN=$1
 TEST_DIR=$2
 for i in $(find $TEST_DIR -name 'тест_*.борщ'); do
-    $INTERPRETER_BIN run --file "$i"
+    $INTERPRETER_BIN "$i"
     exit_code=$?
     [ $exit_code -eq 0 ] || exit $exit_code
     echo "$i" - Success
